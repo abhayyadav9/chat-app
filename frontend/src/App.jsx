35c7d1c9daab0db1af18b15a3e1bdbeae87637cc
Abhay yadav <yadavabhay8227.com>
2024-04-08 01:17:06 +0530

    create all ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,14 @@ import './App.css'
 import { Login } from './pages/login/Login'
 import { Home } from './pages/home/Home'
 import  SignUp from './pages/signup/SignUp'
+import Sidebar from './components/sidebar/Sidebar'
 
 function App() {
 
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      
-<SignUp/>     
-      
+      <Home/>
    </div>
   )
 }
---
+++ frontend/src/components/sidebar/messages/Message.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Message() {
+  return (
+    <div  className='chat chat-end'>
+        <div className=' chat-image avatar'>
+            <div className=' w-10 rounded-full'>
+            <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+
+
+            </div>
+            
+        </div>
+        <div className={` chat-bubble text-white bg-red-500`}>
+Hi! whatsupp
+        </div>
+        <div className=' chat-footer opacity-50 text-xs flex gap-1 items-center'>12:40</div>
+
+
+    </div>
+  )
+}
+
+export default Message
